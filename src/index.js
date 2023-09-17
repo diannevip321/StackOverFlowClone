2b7500c2aa43a583ffb0ce7ca916dce6f3221f7b
Dianne <dianne.vincent@stonybrook.edu>
2023-09-17 11:21:20 -0400

    Updated First Commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import Model from './model.js';
 
 window.onload = function() {
   // fill me with relevant code
-};
+  document.getElementById('questions').onclick = function() {
+    console.log('Questions clicked');
+  };
+
+  document.getElementById('tags').onclick = function() {
+    console.log('Tags clicked');
+  };
+  
+}
