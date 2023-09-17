4649c5e9c37dbdfc7f1537f22daf97ed898b4ce1
Dianne <dianne.vincent@stonybrook.edu>
2023-09-17 19:24:34 -0400

    Ask Question Form Update

--- src/index.js
+++ src/index.js
@@ -1,36 +1,43 @@
-import Model from './model.js';
-import Question from './question.js';
+import Model from "./model.js";
+import Question from "./question.js";
 
-window.onload = function() {
+window.onload = function () {
   // fill me with relevant code
-  document.getElementById('questions').onclick = function() {
-    console.log('Questions clicked');
+  document.getElementById("questions").onclick = function () {
+    console.log("Questions clicked");
   };
 
-  document.getElementById('tags').onclick = function() {
-    console.log('Tags clicked');
-  };
-  
-  document.getElementById('ask_question').onclick = function() {
-    console.log('Ask question clicked');
+  document.getElementById("tags").onclick = function () {
+    console.log("Tags clicked");
   };
 
+  document.getElementById("ask_question").onclick = function () {
+    console.log("Ask question clicked");
+  };
+
+  // method to creaete an instace of the model specifically for the ask-question form
+  // method pushes the form data into the model
   function askQuestion(event) {
-    console.log(document.getElementById('fname').value);
-    console.log(document.getElementById('lname').value);
+    console.log(document.getElementById("qform_title").value);
+    console.log(document.getElementById("qform_text").value);
+    console.log(document.getElementById("qform_tags").value);
+    console.log(document.getElementById("qform_username").value);
     console.log(Date.now());
 
-    //creating an instance of the model
     const model = new Model();
-    model.data.questions.push({askedBy: document.getElementById('fname').value, askDate: new Date(Date.now())} );
+    model.data.questions.push(
+      {
+        askedBy: document.getElementById("qform_username").value,
+        askDate: new Date(Date.now()),
+        title: document.getElementById("qform_title").value,
+        text: document.getElementById("qform_text").value
+      }
+    );
     console.log(model.data.questions);
-    
 
     event.preventDefault();
-    
   }
 
-  
-  
-  
-}
+  const ask_question_form = document.getElementById("ask_question_form");
+  ask_question_form.addEventListener("submit", askQuestion);
+};
